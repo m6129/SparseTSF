39a5687489344ad516d06f9ef10bb08705271026
Anton <104712265+m6129@users.noreply.github.com>
2024-05-10 19:14:57 +0300

    add new benncmark script

---
+++ scripts/SparseTSF/multivariate/de_big.sh
@@ -0,0 +1,34 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+
+model_name=SparseTSF
+
+root_path_name=./dataset/
+data_path_name=de_big.csv
+model_id_name=de_big
+data_name=custom
+
+seq_len=720
+for pred_len in 24 96 192 336 720
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name'_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --period_len 4 \
+    --enc_in 16 \
+    --train_epochs 30 \
+    --patience 5 \
+    --itr 1 --batch_size 256 --learning_rate 0.02> logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
+done
